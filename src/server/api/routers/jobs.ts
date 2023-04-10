0d9023cb55a5f2cf17d46ad66fe88040e26b0687
shan5742 <asamshan456@gmail.com>
2023-04-10 23:35:51 +0100

    add mutation for job listing

--- src/server/api/routers/jobs.ts
+++ src/server/api/routers/jobs.ts
@@ -7,11 +7,14 @@ export const jobRouter = createTRPCRouter({
     return ctx.prisma.jobListing.findMany();
   }),
     create: protectedProcedure
-    .input(z.object({ title: z.string() }))
+    .input(z.object({ title: z.string(), description: z.string(), requirements: z.string(), location: z.string() }))
     .mutation(({ ctx, input }) => {
       return ctx.prisma.jobListing.create({
         data: {
           title: input.title,
+          description: input.description,
+          requirements: input.requirements,
+          location: input.location,
           userId: ctx.session.user.id,
         },
       });
