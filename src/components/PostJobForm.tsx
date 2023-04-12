ce85c4d018e40ccbb346dabd27b18cf730ab6bb8
shan5742 <asamshan456@gmail.com>
2023-04-12 20:59:06 +0100

    general cleanup

--- src/components/PostJobForm.tsx
+++ src/components/PostJobForm.tsx
@@ -48,37 +48,71 @@ export const PostJobForm: React.FC = () => {
         <label className="label">
           <span className="label-text">Company</span>
         </label>
-        <input value={formState.company} onChange={e => setFormState({ ...formState, company: e.target.value })} type="text" placeholder="Type here" className="input input-bordered w-full outline-none" />
+        <input
+          value={formState.company}
+          onChange={e => setFormState({ ...formState, company: e.target.value })}
+          type="text" placeholder="e.g. Apple"
+          className="input input-bordered w-full outline-none"
+        />
       </div>
       <div className="form-control w-full">
         <label className="label">
           <span className="label-text">Job title</span>
         </label>
-        <input value={formState.title} onChange={e => setFormState({ ...formState, title: e.target.value })} type="text" placeholder="Type here" className="input input-bordered w-full outline-none" />
+        <input
+          value={formState.title}
+          onChange={e => setFormState({ ...formState, title: e.target.value })}
+          type="text"
+          placeholder="e.g. Software Engineer"
+          className="input input-bordered w-full outline-none"
+        />
       </div>
       <div className="form-control w-full">
         <label className="label">
           <span className="label-text">Salary</span>
         </label>
-        <input value={formState.salary} onChange={e => setFormState({ ...formState, salary: +e.target.value })} type="number" placeholder="Type here" className="input input-bordered w-full outline-none" />
+        <input
+          value={formState.salary}
+          onChange={e => setFormState({ ...formState, salary: +e.target.value })}
+          type="number"
+          placeholder="e.g. 100000"
+          className="input input-bordered w-full outline-none"
+        />
       </div>
       <div className="form-control w-full">
         <label className="label">
           <span className="label-text">Job description?</span>
         </label>
-        <input value={formState.description} onChange={e => setFormState({ ...formState, description: e.target.value })} type="text" placeholder="Type here" className="input input-bordered w-full outline-none" />
+        <input
+          value={formState.description}
+          onChange={e => setFormState({ ...formState, description: e.target.value })}
+          type="text"
+          placeholder="e.g. Full stack developer"
+          className="input input-bordered w-full outline-none"
+        />
       </div>
       <div className="form-control w-full">
         <label className="label">
           <span className="label-text">Location</span>
         </label>
-        <input value={formState.location} onChange={e => setFormState({ ...formState, location: e.target.value })} type="text" placeholder="Type here" className="input input-bordered w-full outline-none" />
+        <input
+          value={formState.location}
+          onChange={e => setFormState({ ...formState, location: e.target.value })}
+          type="text"
+          placeholder="e.g. Remote"
+          className="input input-bordered w-full outline-none"
+        />
       </div>
       <div className="form-control">
         <label className="label">
           <span className="label-text">Requirements</span>
         </label>
-        <textarea value={formState.requirements} onChange={e => setFormState({ ...formState, requirements: e.target.value })} className="textarea textarea-bordered h-24 outline-none" placeholder="Job requirements"></textarea>
+        <textarea
+          value={formState.requirements}
+          onChange={e => setFormState({ ...formState, requirements: e.target.value })}
+          className="textarea textarea-bordered h-24 outline-none"
+          placeholder="e.g. 3 years experience"
+        />
       </div>
       <div className="form-control">
         <label className="label cursor-pointer">
--- src/pages/post-job.tsx
+++ src/pages/post-job.tsx
@@ -14,7 +14,9 @@ const PostJob: NextPage = () => {
       <Header />
       <div className="p-16 flex flex-col align-center items-center w-screen justify-center max-w-6xl mx-auto">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-slate-900 md:text-5xl lg:text-6xl dark:text-white">Post a Job</h1>
-        <p className="mb-6 text-lg font-normal text-slate-500 lg:text-xl sm:px-16 xl:px-48 dark:text-slate-400 text-center">Remember to get the best quality candidates, be as descriptive as possible with your job descriptions and requirements..</p>
+        <p className="mb-6 text-lg font-normal text-slate-500 lg:text-xl sm:px-16 xl:px-48 dark:text-slate-400 text-center">
+          Remember to get the best quality candidates, be as descriptive as possible with your job descriptions and requirements.
+        </p>
       </div>
       <PostJobForm />
     </main>
